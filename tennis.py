537eb5987f90e561e34bee8bebbb8dfafd23dbf2
tjkemp <tero.kemppi@gmail.com>
2020-04-15 20:10:54 +0300

    add project starting point code

---
+++ tennis.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from unityagents import UnityEnvironment
+import numpy as np
+
+
+def print_env_info(env):
+
+    # get the default brain
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    # reset the environment
+    env_info = env.reset(train_mode=True)[brain_name]
+
+    # number of agents
+    num_agents = len(env_info.agents)
+    print('Number of agents:', num_agents)
+
+    # size of each action
+    action_size = brain.vector_action_space_size
+    print('Size of each action:', action_size)
+
+    # examine the state space
+    states = env_info.vector_observations
+    state_size = states.shape[1]
+    print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('The state for the first agent looks like:', states[0])
+
+
+def random_play(env):
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+
+    for i in range(5):                                         # play game for 5 episodes
+        env_info = env.reset(train_mode=False)[brain_name]     # reset the environment
+        states = env_info.vector_observations                  # get the current state (for each agent)
+        num_agents = len(env_info.agents)
+        action_size = brain.vector_action_space_size
+        scores = np.zeros(num_agents)                          # initialize the score (for each agent)
+
+        while True:
+            # select an action (for each agent)
+            actions = np.random.randn(num_agents, action_size)
+            actions = np.ones((num_agents, action_size)) * 0.1
+            actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+            env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+            next_states = env_info.vector_observations         # get next state (for each agent)
+            rewards = env_info.rewards                         # get reward (for each agent)
+            dones = env_info.local_done                        # see if episode finished
+            scores += env_info.rewards                         # update the score (for each agent)
+            states = next_states                               # roll over states to next time step
+            if np.any(dones):                                  # exit loop if episode finished
+                break
+        print('Total score (averaged over agents) this episode: {}'.format(np.mean(scores)))
+
+
+def main():
+    env = UnityEnvironment(file_name="./Tennis_Linux/Tennis.x86_64")
+    print_env_info(env)
+    random_play(env)
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
